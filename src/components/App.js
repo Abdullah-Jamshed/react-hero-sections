e1ea7b55863e9f84f375e3fbbd8174349cada2b1
abdullah jamshed <abdullahjamshad01@gmail.com>
2021-06-01 18:06:49 +0500

    setup environment

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import "../styles/App.css";
+
+const App = () => {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Header</h1>
+      </header>
+    </div>
+  );
+};
+
+export default App;
