552f1fe4c068403d1b8f76ae0e84bb9a25ea4885
abdullah jamshed <abdullahjamshad01@gmail.com>
2021-06-20 17:09:17 +0500

    css to scss  heroSection1

--- src/components/HeroSection1.js
+++ src/components/HeroSection1.js
@@ -1,34 +1,34 @@
 import React from "react";
 import Image from "../images/herobg.svg";
 
-import "../styles/herosection1.css";
+import styles from "../styles/scss/heroSection1.module.scss";
 
 const HeroSection1 = () => {
   const text = `Lorem ips o eum illo cupiditate accusamus, fugiat nostrum quaerat. Dignissimos, officiis error amet fuga enim sequi. Suscipit ratione odit labore cum, praesentium sequi!`;
   const words = text.split(" ");
   return (
-    <div className='heroSection'>
-      <div className='heroContent'>
+    <div className={`${styles.heroSection}`}>
+      <div className={`${styles.heroContent}`}>
         <div>
-          <h1 style={{ fontSize: "40px", marginBottom: "10px" }} className='animated fadeIn'>
+          <h1 style={{ fontSize: "40px", marginBottom: "10px", fontSize: "4rem" }} className={`${(styles.animated, styles.fadeIn)} `}>
             Heading
           </h1>
           <h2 style={{ marginBottom: "20px" }}>
             {"Lorem ips o eum illo cupiditate accusamus, ".split(" ").map((word, i) => (
-              <span className='animated fadeIn' style={{ animationDelay: `${i * 0.1}s` }}>
+              <span className={`${styles.animated} ${styles.fadeIn} `} style={{ animationDelay: `${i * 0.1}s` }}>
                 {word + ` `}
               </span>
             ))}
           </h2>
-          <p className='text'>
+          <p className={`${styles.text}`}>
             {words.map((word, i) => (
-              <span key={i} className='animated fadeIn' style={{ animationDelay: `${i * 0.1}s` }}>
+              <span key={i} className={`${styles.animated} ${styles.fadeIn} `} style={{ animationDelay: `${i * 0.1}s` }}>
                 {word + ` `}
               </span>
             ))}
           </p>
         </div>
-        <div className='imageContainer'>
+        <div className={`${styles.imageContainer}`}>
           <img src={Image} alt='bg' />
         </div>
       </div>
--- src/pages/Section1.js
+++ src/pages/Section1.js
@@ -9,41 +9,44 @@ const Section1 = () => {
       <HeroSection1 />
 
       <div style={{ padding: "50px", textAlign: "justify" }}>
-        <h1 style={{ textAlign: "center", padding: "20px" }}>Lorem ipsum</h1>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores debitis consequuntur voluptate! Iure ullam sint sit animi temporibus dicta
-        saepe blanditiis dolorem fugiat accusamus architecto perferendis dignissimos, eveniet inventore reiciendis? Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Maiores debitis consequuntur voluptate! Iure ullam sint sit animi temporibus dicta saepe blanditiis dolorem
-        fugiat accusamus architecto perferendis dignissimos, eveniet inventore reiciendis? Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Maiores debitis consequuntur voluptate! Iure ullam sint sit animi temporibus dicta saepe blanditiis dolorem fugiat accusamus architecto
-        perferendis dignissimos, eveniet inventore reiciendis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores debitis consequuntur
-        voluptate! Iure ullam sint sit animi temporibus dicta saepe blanditiis dolorem fugiat accusamus architecto perferendis dignissimos, eveniet
-        inventore reiciendis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores debitis consequuntur voluptate! Iure ullam sint sit
-        dignissimos, eveniet inventore reiciendis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores debitis consequuntur voluptate!
-        Iure ullam sint sit animi temporibus dicta saepe blanditiis dolorem fugiat accusamus architecto perferendis dignissimos, eveniet inventore
-        reiciendis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores debitis consequuntur voluptate! Iure ullam sint sit animi
-        temporibus dicta saepe blanditiis dolorem fugiat accusamus architecto perferendis dignissimos, eveniet inventore reiciendis? Lorem ipsum dolor
-        sit amet consectetur adipisicing elit. Maiores debitis consequuntur voluptate! Iure ullam sint sit animi temporibus dicta saepe blanditiis
-        dolorem fugiat accusamus architecto perferendis dignissimos, eveniet inventore reiciendis? Lorem ipsum dolor sit amet consectetur adipisicing
-        elit. Maiores debitis consequuntur voluptate! Iure ullam sint sit animi temporibus dicta saepe blanditiis dolorem fugiat accusamus architecto
-        temporibus dicta saepe blanditiis dolorem fugiat accusamus architecto perferendis dignissimos, eveniet inventore reiciendis? Lorem ipsum dolor
-        sit amet consectetur adipisicing elit. Maiores debitis consequuntur voluptate! Iure ullam sint sit animi temporibus dicta saepe blanditiis
-        dolorem fugiat accusamus architecto perferendis dignissimos, eveniet inventore reiciendis? Lorem ipsum dolor sit amet consectetur adipisicing
-        elit. Maiores debitis consequuntur voluptate! Iure ullam sint sit animi temporibus dicta saepe blanditiis dolorem fugiat accusamus architecto
-        perferendis dignissimos, eveniet inventore reiciendis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores debitis consequuntur
-        voluptate! Iure ullam sint sit animi temporibus dicta saepe blanditiis dolorem fugiat accusamus architecto perferendis dignissimos, eveniet
-        inventore reiciendis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores debitis consequuntur voluptate! Iure ullam sint sit
-        animi temporibus dicta saepe blanditiis dolorem fugiat accusamus architecto perferendis dignissimos, eveniet inventore reiciendis? Lorem ipsum
-        dolor sit amet consectetur adipisicing elit. Maiores debitis consequuntur voluptate! Iure ullam sint sit animi temporibus dicta saepe
-        blanditiis dolorem fugiat accusamus architecto perferendis dignissimos, eveniet inventore reiciendis? Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Maiores debitis consequuntur voluptate! Iure ullam sint sit animi temporibus dicta saepe blanditiis dolorem fugiat accusamus
-        architecto perferendis dignissimos, eveniet inventore reiciendis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores debitis
-        consequuntur voluptate! Iure ullam sint sit animi temporibus dicta saepe blanditiis dolorem fugiat accusamus architecto perferendis
-        dignissimos, eveniet inventore reiciendis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores debitis consequuntur voluptate!
-        Iure ullam sint sit animi temporibus dicta saepe blanditiis dolorem fugiat accusamus architecto perferendis dignissimos, eveniet inventore
-        reiciendis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores debitis consequuntur voluptate! Iure ullam sint sit animi
-        temporibus dicta saepe blanditiis dolorem fugiat accusamus architecto perferendis dignissimos, eveniet inventore reiciendis? Lorem ipsum dolor
-        sit amet consectetur adipisicing elit. Maiores debitis consequuntur voluptate! Iure ullam sint sit animi temporibus dicta saepe blanditiis
-        dolorem fugiat accusamus architecto perferendis dignissimos, eveniet inventore reiciendis?
+        <h1 style={{ textAlign: "center", padding: "20px", fontSize: "3rem" }}>Lorem ipsum</h1>
+        <p style={{ fontSize: "1.6rem" }}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores debitis consequuntur voluptate! Iure ullam sint sit animi temporibus dicta
+          saepe blanditiis dolorem fugiat accusamus architecto perferendis dignissimos, eveniet inventore reiciendis? Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Maiores debitis consequuntur voluptate! Iure ullam sint sit animi temporibus dicta saepe blanditiis dolorem
+          fugiat accusamus architecto perferendis dignissimos, eveniet inventore reiciendis? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Maiores debitis consequuntur voluptate! Iure ullam sint sit animi temporibus dicta saepe blanditiis dolorem fugiat accusamus architecto
+          perferendis dignissimos, eveniet inventore reiciendis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores debitis consequuntur
+          voluptate! Iure ullam sint sit animi temporibus dicta saepe blanditiis dolorem fugiat accusamus architecto perferendis dignissimos, eveniet
+          inventore reiciendis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores debitis consequuntur voluptate! Iure ullam sint sit
+          dignissimos, eveniet inventore reiciendis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores debitis consequuntur voluptate!
+          Iure ullam sint sit animi temporibus dicta saepe blanditiis dolorem fugiat accusamus architecto perferendis dignissimos, eveniet inventore
+          reiciendis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores debitis consequuntur voluptate! Iure ullam sint sit animi
+          temporibus dicta saepe blanditiis dolorem fugiat accusamus architecto perferendis dignissimos, eveniet inventore reiciendis? Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Maiores debitis consequuntur voluptate! Iure ullam sint sit animi temporibus dicta saepe
+          blanditiis dolorem fugiat accusamus architecto perferendis dignissimos, eveniet inventore reiciendis? Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Maiores debitis consequuntur voluptate! Iure ullam sint sit animi temporibus dicta saepe blanditiis dolorem fugiat
+          accusamus architecto temporibus dicta saepe blanditiis dolorem fugiat accusamus architecto perferendis dignissimos, eveniet inventore
+          reiciendis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores debitis consequuntur voluptate! Iure ullam sint sit animi
+          temporibus dicta saepe blanditiis dolorem fugiat accusamus architecto perferendis dignissimos, eveniet inventore reiciendis? Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Maiores debitis consequuntur voluptate! Iure ullam sint sit animi temporibus dicta saepe
+          blanditiis dolorem fugiat accusamus architecto perferendis dignissimos, eveniet inventore reiciendis? Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Maiores debitis consequuntur voluptate! Iure ullam sint sit animi temporibus dicta saepe blanditiis dolorem fugiat
+          accusamus architecto perferendis dignissimos, eveniet inventore reiciendis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+          debitis consequuntur voluptate! Iure ullam sint sit animi temporibus dicta saepe blanditiis dolorem fugiat accusamus architecto perferendis
+          dignissimos, eveniet inventore reiciendis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores debitis consequuntur voluptate!
+          Iure ullam sint sit animi temporibus dicta saepe blanditiis dolorem fugiat accusamus architecto perferendis dignissimos, eveniet inventore
+          reiciendis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores debitis consequuntur voluptate! Iure ullam sint sit animi
+          temporibus dicta saepe blanditiis dolorem fugiat accusamus architecto perferendis dignissimos, eveniet inventore reiciendis? Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Maiores debitis consequuntur voluptate! Iure ullam sint sit animi temporibus dicta saepe
+          blanditiis dolorem fugiat accusamus architecto perferendis dignissimos, eveniet inventore reiciendis? Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Maiores debitis consequuntur voluptate! Iure ullam sint sit animi temporibus dicta saepe blanditiis dolorem fugiat
+          accusamus architecto perferendis dignissimos, eveniet inventore reiciendis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+          debitis consequuntur voluptate! Iure ullam sint sit animi temporibus dicta saepe blanditiis dolorem fugiat accusamus architecto perferendis
+          dignissimos, eveniet inventore reiciendis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores debitis consequuntur voluptate!
+          Iure ullam sint sit animi temporibus dicta saepe blanditiis dolorem fugiat accusamus architecto perferendis dignissimos, eveniet inventore
+          reiciendis?
+        </p>
       </div>
     </>
   );
